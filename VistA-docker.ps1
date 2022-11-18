309748d0a9b6e5c51d4baa26df2df0a3f527fa85
Raman Sailopal <thelinuxguys@yahoo.com>
2022-11-18 14:15:49 +0000

    Update VistA-docker.ps1
    Added restart option

--- VistA-docker.ps1
+++ VistA-docker.ps1
@@ -1,6 +1,36 @@
 #
 #   Check to see if Docker is installed. If not, download and install Docker Desktop for Windows. Then pull and run VistA EHR image in container
 #
+param ([String] $restart="N")
+if ($restart.ToUpper() -eq "Y") {
+    try {
+        $contid=""
+        if (docker ps -a | Select-String "wv") { 
+            docker ps -a | Select-String "wv" | ForEach-Object { 
+                $bits=$_.toString().split(" ")
+                if ($bits[0] -notmatch "CONTAINER") { 
+                    $contid = $bits[0] 
+                } 
+                docker rm -f $contid | Out-Null
+                docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr | Out-Null
+                Write-Host -ForegroundColor green "VistA EHR is now running in Docker. Use the install.ps1 script to install the client CPRS software if needed"
+                exit
+            }
+        }
+        if ($contid -eq "") {
+            $ans3 = Read-Host -Prompt "There was an error recreating the VistA EHR container. Do you wish to create one now? (Y/N)"
+            if ($ans3.ToUpper() -eq "Y") {
+                docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr | Out-Null
+                Write-Host -ForegroundColor green "VistA EHR is now running in Docker. Use the install.ps1 script to install the client CPRS software if needed"
+                exit
+            }
+        }
+    }
+    catch {
+        Write-Host -ForegroundColor Red "There was an error recreating the VistA EHR container"
+    }
+    exit
+}
 try {
     docker version
     Write-Host -ForegroundColor green "Docker Desktop already installed"
@@ -9,20 +39,21 @@ try {
         if (docker ps -a | Select-String "wv") {
             $ans2=Read-Host -Prompt "Would you like to remove the existing and recreate a container running VistA EHR? (Y/N)"
             if ( $ans2.ToUpper() -eq "Y") {
-                docker ps -a | ForEach-Object { 
-                    $bits=$_.split(" ")
+                docker ps -a | Select-String "wv" | ForEach-Object { 
+                    $bits=$_.toString().split(" ")
                     if ($bits[0] -notmatch "CONTAINER") { 
                         $contid = $bits[0] 
                     } 
                 }
-                docker rm -f $contid
-                docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr
+                docker rm -f $contid | Out-Null
+                docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr | Out-Null
                 Write-Host -ForegroundColor green "VistA EHR is now running in Docker. Use the install.ps1 script to install the client CPRS software if needed"
                 exit
             }
         }
-        docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr
+        docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr | Out-Null
         Write-Host -ForegroundColor green "VistA EHR is now running in Docker. Use the install.ps1 script to install the client CPRS software if needed"
+        exit
     }
 }
 catch {
@@ -41,7 +72,7 @@ catch {
             Write-Host -ForegroundColor green "Running Docker Desktop"
             & '~\Docker Desktop.lnk'
             Write-Host -ForegroundColor green "Running VistA EHR in Docker"
-            docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr
+            docker run -d -p 2222:22 -p 8001:8001 -p 8080:8080 -p 9430:9430 -p 9080:9080 --name=wv worldvista/worldvista-ehr | Out-Null
             Write-Host -ForegroundColor green "VistA EHR is now running in Docker. Use the install.ps1 script to install the client CPRS software if needed"
         }
         catch {
